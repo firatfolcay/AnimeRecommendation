f12be404fb6811782d227e1d3e696091db7abe0a
Fırat Fuat Olcay <40544125+firatfolcay@users.noreply.github.com>
2023-04-26 15:26:29 +0300

    Update functions.py
    Functions.py updated for the future functional structure.

--- functions.py
+++ functions.py
@@ -1,17 +1,25 @@
 import pandas as pd
 
 # Load anime dataframe
-animes_df = pd.read_csv('anime.csv')
 
-# Get unique genres
-unique_genres = sorted(set(g for genres in animes_df['genre'].fillna('') for g in genres.split(',')))
+def getOneHot(animes_df):
+    
+    # Get unique genres
+    unique_genres = sorted(set(g for genres in animes_df['genre'].fillna('') for g in genres.split(',')))
 
-# One-hot encode genres
-for value in unique_genres:
-    animes_df[value] = animes_df['genre'].str.contains(value, case=False).fillna(False).astype(int)
+    # One-hot encode genres
+    for value in unique_genres:
+        animes_df[value] = animes_df['genre'].str.contains(value, case=False).fillna(False).astype(int)
 
-# Drop the original genre column
-animes_df = animes_df.drop('genre', axis=1)
+    # Drop the original genre column
+    animes_df = animes_df.drop('genre', axis=1)
+    
+    return animes_df
+    
 
-# Save the encoded dataframe to a CSV file
-animes_df.to_csv('anime_encoded.csv', index=False)
+if __name__ == "__main__":
+    
+    animes_df = getOneHot(pd.read_csv("anime.csv"))
+    
+    # Save the encoded dataframe to a CSV file
+    animes_df.to_csv('anime_encoded.csv', index=False)
