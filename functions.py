0a1db517a23c3645749afaf75534200d1930b260
agirnob <kakcayigitbaran@gmail.com>
2023-04-29 15:07:04 +0300

    added the function multiply one-hot with rating

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
+
 # Load anime dataframe
 
 def getOneHot(animes_df):
-    
     # Get unique genres
     unique_genres = sorted(set(g for genres in animes_df['genre'].fillna('') for g in genres.split(',')))
 
@@ -13,17 +13,26 @@ def getOneHot(animes_df):
 
     # Drop the original genre column
     animes_df = animes_df.drop('genre', axis=1)
-    
+
     return animes_df
+
+
 def csvToVector(dataframe):
-    testframe = dataframe.loc[:, "Adventure":"Yaoi"]
     list_of_lists = [[row[col] for col in dataframe.loc[:, 'Action':'Yaoi'].columns] for _, row in dataframe.iterrows()]
-    return list_of_lists
+    result_df = pd.DataFrame(list_of_lists, columns=dataframe.loc[:, 'Action':'Yaoi'].columns)
+    result_list = multiByRating(dataframe, result_df)
+    return result_list
+
+def multiByRating(dataframe, oneHotVector):
+    ratings = dataframe['rating'].fillna(6.5)
+    result_df = oneHotVector.mul(ratings, axis=0)
+    return result_df.values.tolist()
 
 if __name__ == "__main__":
-
+    pd.set_option('display.max_columns', None)
     # CHECK: if the csv file getting from the right place
-    animes_df = getOneHot(pd.read_csv("/home/agirnob/Downloads/anime archive/anime.csv"))
-    csvToVector(animes_df)
+    animes_df = getOneHot(pd.read_csv("anime.csv"))
+    # oneHotVector = csvToVector(animes_df)
+    print(csvToVector(animes_df))
     # Save the encoded dataframe to a CSV file
     animes_df.to_csv('anime_encoded.csv', index=False)
