1df5a5ca971fd18c6e12a11f84842ec26825b74b
Fırat Fuat Olcay <40544125+firatfolcay@users.noreply.github.com>
2023-05-01 00:22:10 +0300

    calcGenreBased function added
    First method is nearly finished.

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 import pandas as pd
-
-
+import numpy as np
+from scipy.spatial.distance import cdist
 # This Function generate unique genres and add to dataframe and assing 1 with corresponding genres
 # input: dataframe from anime.csv
 # output: dataframe with onehots encodings
@@ -36,11 +36,39 @@ def multiByRating(dataframe, oneHotVector):
     return result_df.values.tolist()
 
 
+# This Function is for determining genre in later steps because we coded genre alphabeticly rather than given order
+# input: dataframe with one hot encodings
+# output: alphabeticly indexed genre array
+def getIndexDict(dataframe):
+    genreArray=[]
+    index=0
+    for genre in animes_df.loc[:,'Action':'Yaoi'].columns:
+        genreArray.append(genre)
+        index+=1
+    return genreArray
+
+
+# This Function applies genre based method
+# input: npUserAnime is array list of indeces, npOneHotMatrix is our onehot list with scores, topXAnime is how many anime will be shown
+# output: recommended anime indices
+def calcGenreBased(npUserAnime, npOneHotMatrix, topXAnime):
+    mean_point = np.mean(npOneHotMatrix[npUserAnime].reshape(len(npUserAnime[0]),40), axis=0)
+    distances = cdist(npOneHotMatrix, np.array([mean_point]), metric='euclidean')
+    closest_point_indices = np.argsort(distances, axis=0)[:topXAnime, 0]
+    #closest_points = npOneHotMatrix[closest_point_indices]
+    return closest_point_indices
+
 if __name__ == "__main__":
     pd.set_option('display.max_columns', None)
     # CHECK: if the csv file getting from the right place
     animes_df = getOneHot(pd.read_csv("anime.csv"))
-    # oneHotVector = csvToVector(animes_df)
-    print(csvToVector(animes_df))
+    
+    oneHotVector = csvToVector(animes_df)
+    npOneHot= np.array(oneHotVector)
+    genreArray=getIndexDict(animes_df)
+    
+    ans= calcGenreBased([[[43],[62],[131],[327],[10991]]] , npOneHot, 5) # Örnek
+    print(animes_df.loc[ans,'name'])
+    #print(csvToVector(animes_df))
     # Save the encoded dataframe to a CSV file
     animes_df.to_csv('anime_encoded.csv', index=False)
